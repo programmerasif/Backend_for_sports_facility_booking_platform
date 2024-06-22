910a472e570e1f9cee792fa652c256d1b7d98979
programmerasif <mdasifkhan3757@gmail.com>
2024-06-22 11:23:49 +0600

    finally done

--- src/app/modules/Auth/auth.controler.ts
+++ src/app/modules/Auth/auth.controler.ts
@@ -1,17 +1,18 @@
 import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
-import sendResponse from '../../utils/sendResponse';
 import { AuthServices } from './auth.service';
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
   const { _id, name, email, role, phone, address } = result.user;
   const sendData = { _id, name, email, role, phone, address };
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
+  const token = result.accesToken;
+
+  res.status(httpStatus.OK).json({
     success: true,
+    statusCode: httpStatus.OK,
     message: 'User logged in successfully',
-    token: result.accesToken,
+    token: token,
     data: sendData,
   });
 });
--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -4,7 +4,6 @@ const sendResponse = <T>(res:Response,data: {
     statusCode: number,
     success:boolean,
     message? : string,
-    token? : string,
     data:T
 })=>{
 res.status(data?.statusCode).json({
